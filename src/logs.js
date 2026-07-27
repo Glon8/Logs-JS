9d0b476ecea270310816b72bd076ab69988812db
Glon8 <root@localhost.localdomain>
2026-07-27 14:23:20 +0000

    typos fix and log func extention

--- src/logs.js
+++ src/logs.js
@@ -45,28 +45,37 @@ const getInfo = () => {
    return {fileName, logLine};
 }
 
-export const log = (tag, ...args) => {
-   const modifiers = [];
+export const log = (...args) => {
+   if(args.length == 1 &&
+   args[0] !== null &&
+   typeof args[0] === 'object' &&
+   !Array.isArray(args[0]) && 
+   Object.keys(args[0]).length > 0) {
+      const modifiers = [];
+      const tag = args[0].tag;
+      const data = args[0].data;
 
-   if(config.timestamp) modifiers.push(`[${getTime()}]`);
-   
-   if (tag == 'i' || tag == 'w' || tag == 'e') {
-      // info | warning | error
-      if (tag === 'i') modifiers.push(`[INFO]`);
-      else if (tag === 'w') modifiers.push(`[WARNING]`);
-      else modifiers.push(`[ERROR]`);
+      if(config.timestamp) modifiers.push(`[${getTime()}]`);
+
+      if (tag == 'i' || tag == 'w' || tag == 'e') {
+         // info | warning | error
+         if (tag === 'i') modifiers.push(`[INFO]`);
+         else if (tag === 'w') modifiers.push(`[WARNING]`);
+         else modifiers.push(`[ERROR]`);
+      }
+      else if (typeof tag == 'string') modifiers.push(`[${tag}]`); 
+
+      if(config.path || config.line){
+         const logInfo = getInfo();
+
+         if(config.path) modifiers.push(`[${logInfo.fileName}]`);
+
+         if(config.line) modifiers.push(`[${logInfo.logLine}]`);
+      }
+
+      modifiers.push(data);
+
+      console.log(...modifiers);
    }
-   else if (typeof tag == 'string') modifiers.push(`[${type}]`); 
-
-   if(config.path || config.line){
-      const logInfo = getInfo();
-
-      if(config.path) modifiers.push(`[${logInfo.fileName}]`);
-      
-      if(config.line) modifiers.push(`[${logInfo.logLine}]`);
-   }
-
-   modifiers.push(...args);
-  
-   console.log(...modifiers)
+   else console.log(...args);
 }
