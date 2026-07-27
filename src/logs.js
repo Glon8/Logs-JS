2e923042d410b894516ac37e6e503587b55978cf
Glon8 <root@localhost.localdomain>
2026-07-27 12:03:39 +0000

    typos fix 3

--- src/logs.js
+++ src/logs.js
@@ -24,7 +24,7 @@ export const setProperties = (timestamp, path, line) => {
 const getTime = () => {
    const date = new Date();
 
-   const time = String(now.getHours()).padStart(2,'0') + ':' + String(now.getMinutes()).padStart(2,'0') + ':' String(now.getSeconds()).padStart(2,'0');
+   const time = String(now.getHours()).padStart(2,'0') + ':' + String(now.getMinutes()).padStart(2,'0') + ':' + String(now.getSeconds()).padStart(2,'0');
 
    return time;
 }
